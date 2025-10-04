e3e52f69046d277b01fca6a1582c0726863199a1
sakab257 <slmbouskine93@gmail.com>
2025-10-04 22:00:54 +0200

    correction des apostrophes

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="h-full w-full flex flex-col items-center gap-6">
       <p className="uppercase font-light tracking-[0.25rem]">ai, Software & data engineering</p>
-      <h1 className="text-6xl font-bold text-center">Développeur <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#CBACF9] to-[#D4B7FF]">Full-Stack</span> passionné par l'<span className="text-transparent bg-clip-text bg-gradient-to-r from-[#CBACF9] to-[#D4B7FF]">IA</span> générative & <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#CBACF9] to-[#D4B7FF]">l'architecture</span> des <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#CBACF9] to-[#D4B7FF]">SI</span></h1>
-      <Button label="Avec qui j'ai Travaillé" />
+      <h1 className="text-6xl font-bold text-center">Développeur <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#CBACF9] to-[#D4B7FF]">Full-Stack</span> passionné par l&apos;<span className="text-transparent bg-clip-text bg-gradient-to-r from-[#CBACF9] to-[#D4B7FF]">IA</span> générative & <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#CBACF9] to-[#D4B7FF]">l&apos;architecture</span> des <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#CBACF9] to-[#D4B7FF]">SI</span></h1>
+      <Button label="Avec qui j&apos;ai Travaillé" />
       <div className="w-[350px] flex items-center justify-center gap-16">
         <CompanyCard imageUrl="/company/cdc.svg" />
         <CompanyCard imageUrl="/company/orange.svg" />
